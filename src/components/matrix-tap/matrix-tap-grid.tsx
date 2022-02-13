d075dcca51b942a03ab3c70902f8a20938a8d0fc
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-13 15:05:11 +0000

    Add header text, modify styling

--- src/components/matrix-tap/matrix-tap-grid.tsx
+++ src/components/matrix-tap/matrix-tap-grid.tsx
@@ -1,7 +1,10 @@
 import React from "react"
 import {
-  Grid
+  Box, 
+  Grid,
+  Typography
 } from "@mui/material"
+import { makeStyles } from "@mui/styles"
 import { MatrixTapButton } from "./matrix-tap-button"
 
 interface IMatrixTapGrid {
@@ -15,21 +18,36 @@ interface IMatrixTapGrid {
  */
 export function MatrixTapGrid(props: IMatrixTapGrid) {
   const { matrix } = props
+  const classes = useStyles()
 
   return (
-    <Grid container spacing={1}>
-      {matrix.map((row, rowIndex) => (
-        <Grid key={rowIndex} container item direction="row" alignItems="center" justifyContent="center" spacing={1}>
-          {row.map((col, colIndex) => (
-            <Grid key={`${rowIndex}-${colIndex}`} item>
-              <MatrixTapButton
-                rowIndex={rowIndex}
-                colIndex={colIndex}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      ))}
-    </Grid>
+    <Box>
+      <Typography align="center" className={classes.text}>
+        Tap the buttons to modify the relation
+      </Typography>
+      <Grid container spacing={1}>
+        {matrix.map((row, rowIndex) => (
+          <Grid key={rowIndex} container item direction="row" alignItems="center" justifyContent="center" spacing={1}>
+            {row.map((col, colIndex) => (
+              <Grid key={`${rowIndex}-${colIndex}`} item>
+                <MatrixTapButton
+                  rowIndex={rowIndex}
+                  colIndex={colIndex}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   )
 }
+
+const useStyles = makeStyles((theme: any) => ({
+  text: {
+    fontSize: theme.typography.pxToRem(18),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: theme.typography.pxToRem(22),
+    },
+  }
+ }))
