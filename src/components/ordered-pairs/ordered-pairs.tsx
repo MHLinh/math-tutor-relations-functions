af6d29e32ce88c1831721b4a451373721d2ee8ff
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-13 15:04:43 +0000

    Fix substring slice bug, modify styling

--- src/components/ordered-pairs/ordered-pairs.tsx
+++ src/components/ordered-pairs/ordered-pairs.tsx
@@ -4,49 +4,52 @@ import {
   Typography
 } from "@mui/material"
 import { makeStyles } from "@mui/styles"
+import { computeOrderedPairs } from "utils"
 
 interface IOrderedPairs {
-  pairs: number[][]
+  matrix: number[][]
 }
 
 /**
  * A component displaying the relation as list of ordered pairs.
- * @param props - pairs of numbers that represent the relation
+ * @param props -  matrix representing the relation
  */
 export function OrderedPairs(props: IOrderedPairs) {
-  const { pairs } = props
+  const { matrix } = props
   const classes = useStyles()
+  
+  const pairs = computeOrderedPairs(matrix)
   const tuplePairs = pairs.map(([a, b]) => `(${a},${b})`)
   const stringPairs = tuplePairs.join(", ")
   const divide = tuplePairs.length > 8
   const middle = Math.ceil(tuplePairs.length / 2)
-  const stringPairs1 = tuplePairs.splice(0, middle).join(", ")
-  const stringPairs2 = tuplePairs.splice(-middle).join(", ")
-
+  const stringPairs1 = tuplePairs.slice(0, middle).join(", ")
+  const stringPairs2 = tuplePairs.slice(-middle).join(", ")
   return (
-    <Box className={classes.box}>
+    <Box>
       <Typography align="center" className={classes.text}>
         Relation as a set of ordered pairs:
       </Typography>
-      <Typography align="center">
-        {divide
-          ? <span>{stringPairs1} <br/> {stringPairs2}</span>
-          : stringPairs
-        }
-      </Typography>
+      {tuplePairs.length > 0
+         ? <Typography align="center">
+            {divide
+              ? <span>{stringPairs1} <br/> {stringPairs2}</span>
+              : stringPairs
+            }
+          </Typography>
+         : <Typography align="center">
+            Empty relation
+          </Typography>
+      }
     </Box>
   )
 }
 
 const useStyles = makeStyles((theme: any) => ({
-  box: {
-    paddingBottom: theme.typography.pxToRem(10),
-    paddingTop: theme.typography.pxToRem(10),
-  },
   text: {
     fontSize: theme.typography.pxToRem(18),
     [theme.breakpoints.up("sm")]: {
-      fontSize: theme.typography.pxToRem(24),
+      fontSize: theme.typography.pxToRem(22),
     },
   }
  }))
