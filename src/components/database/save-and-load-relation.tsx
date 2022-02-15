eb5911ebff10224854b1a1c6e7898f6a257c38da
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-15 23:48:16 +0000

    Add save and load relation
    Add components for handling saving and loading relations

---
+++ src/components/database/save-and-load-relation.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import {
+  Box
+} from "@mui/material"
+import { makeStyles } from "@mui/styles"
+import { MatrixContext, IMatrixContext } from "components/matrix-context/matrix-context"
+import { SaveRelation } from "./save-relation"
+import { LoadRelation } from "./load-relation"
+
+interface ISaveAndLoadRelation {
+  matrixContextValue: IMatrixContext, // Matrix context for accessing the matrix and its setter
+  type: string,                       // The type of the relation (for displaying in correct pages)
+}
+
+/**
+ * A component displaying save and load relation components.
+ */
+export function SaveAndLoadRelation(props: ISaveAndLoadRelation) {
+  const { matrixContextValue, type } = props
+  const classes = useStyles()
+
+  return (
+    <Box>
+      <MatrixContext.Provider value={matrixContextValue}>
+        <Box className={classes.box}>
+          <SaveRelation type={type} />
+        </Box>
+        <Box className={classes.box}>
+          <LoadRelation type={type} />
+        </Box>
+      </MatrixContext.Provider>
+    </Box>
+  )
+}
+const useStyles = makeStyles((theme: any) => ({
+  box: {
+    paddingBottom: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+  }
+}))
