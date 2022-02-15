a0532826a60c2e746a24b3e6e4fcf454677e5a83
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-15 22:40:17 +0000

    Add clear button
    Add a button to clear the inputted relation

---
+++ src/components/clear-button/clear-button-provider.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { MatrixContext, IMatrixContext } from "components/matrix-context/matrix-context"
+import { ClearButton } from "./clear-button"
+
+interface IClearButtonProvider {
+  matrixContextValue: IMatrixContext, // Matrix context for accessing the matrix and its setter
+}
+
+/**
+ * A component providing the matrix context to the clear button.
+ * Allows for accessing the matrix and clearing it.
+ */
+export function ClearButtonProvider(props: IClearButtonProvider) {
+  const { matrixContextValue } = props
+
+  return (
+    <MatrixContext.Provider value={matrixContextValue}>
+      <ClearButton />
+    </MatrixContext.Provider>
+  )
+}
