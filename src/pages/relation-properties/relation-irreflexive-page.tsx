8723c9b9b95418dc0de2f0da3d9dfbdaa18e823f
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 12:13:02 +0000

    Add pages for studying relation properties

---
+++ src/pages/relation-properties/relation-irreflexive-page.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import {
+  Box,
+  Container,
+  Button
+} from "@mui/material"
+import { RelationIrreflexive } from "components"
+
+/**
+ * Displays the page for studying the irreflexive property.
+ */
+export function RelationIrreflexivePage() {
+  return (
+    <Container>
+      <RelationIrreflexive />
+    </Container>
+  )
+}
