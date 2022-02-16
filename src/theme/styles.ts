12eb24b990b6a04bca6c2a04ac6f5e34672a5944
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 12:07:34 +0000

    Add custom theme styling

---
+++ src/theme/styles.ts
@@ -0,0 +1,10 @@
+export const center = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+export const paddingStyle = {
+  paddingLeft: "min(20%, 15.625rem)", // 250 px
+  paddingRight: "min(20%, 15.625rem)", // 250 px
+}
---
+++ src/theme/theme.tsx
@@ -0,0 +1,45 @@
+import { 
+  createTheme, 
+  Theme, 
+  ThemeOptions, 
+  Palette 
+} from "@mui/material"
+import { palette } from "./palette"
+
+interface IPalette extends Palette {
+  neutral: {
+    main: string,
+    darker: string,
+    contrastText: string
+  },
+  custom: {
+    green: string,
+    red: string,
+    grey: string,
+    lightGrey: string
+    lightBlue: string,
+    lightGreen: string
+  }
+}
+
+interface IThemeOptions extends ThemeOptions {
+  palette: IPalette;
+}
+
+export const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#C9CCD1",
+      darker: "#656669",
+      contrastText: "#000000"
+    },
+    custom: {
+      green: palette.green,
+      red: palette.red,
+      grey: palette.grey,
+      lightGrey: palette.lightGrey,
+      lightBlue: palette.lightBlue,
+      lightGreen: palette.lightGreen,
+    }
+  },
+} as IThemeOptions)
