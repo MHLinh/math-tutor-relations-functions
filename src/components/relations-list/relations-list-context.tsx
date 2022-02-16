3b6865b7206653a48cf7fd00766051ff50660343
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 12:06:29 +0000

    Add a list displaying saved relations

---
+++ src/components/relations-list/relations-list-context.tsx
@@ -0,0 +1,18 @@
+import React, { createContext } from "react"
+
+
+export interface IRelationsListContext {
+  selected: number,                     // Index of selected relation in list
+  setter: (selected: number) => void    // Function to set the relation
+  deleteFunction: (id: number) => void  // Function to delete the relation
+}
+
+/**
+ * React context used to access the selected relation, 
+ * modify the current relation, and delete saved relations in children components.
+ */
+export const RelationsListContext = createContext<IRelationsListContext>({
+  selected: -1,
+  setter: (selected: number) => {},
+  deleteFunction: (id: number) => {}
+})
