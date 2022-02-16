20512da06748bacb26f76aace04761fda45e3a05
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 14:55:47 +0000

    Add back and home buttons

---
+++ src/components/navigation/back-button.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { useNavigate, useLocation } from "react-router-dom"
+import {
+  IconButton
+} from "@mui/material"
+import ArrowBackIcon from "@mui/icons-material/ArrowBack"
+
+/**
+ * A button to go back to the previous page.
+ */
+export function BackButton() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const goBack = () => {
+    navigate(-1)
+  }
+
+  if(location.pathname !== "/") {
+    return (
+      <IconButton
+        aria-label="back-button"
+        size="large"
+        color="info"
+        onClick={goBack}
+      >
+        <ArrowBackIcon fontSize="inherit" />
+      </IconButton>
+    )
+  }
+  
+  return null
+}
---
+++ src/components/navigation/home-button.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+import {
+  IconButton
+} from "@mui/material"
+import HomeIcon  from "@mui/icons-material/Home"
+
+/**
+ * A button to go the home page.
+ */
+export function HomeButton() {
+
+  return (
+    <IconButton
+      aria-label="back-button"
+      size="large"
+      color="info"
+      component={NavLink}
+      to="/"
+    >
+      <HomeIcon fontSize="inherit" />
+    </IconButton>
+  )
+}
