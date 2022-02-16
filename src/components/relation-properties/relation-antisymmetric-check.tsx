ddf6385d5712ecffce110c5d2aafede266c81d27
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 11:58:57 +0000

    Add relation antisymmetric check

---
+++ src/components/relation-properties/relation-antisymmetric-check.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import {
+  Box,
+  Typography
+} from "@mui/material"
+import { checkAntisymmetric } from "utils"
+
+interface IRelationAntisymmetricCheck {
+ matrix: number[][] // Relation stored as matrix
+}
+
+/**
+ * A component displaying whether the relation 
+ * has pairs that make the relation not antisymmetric.
+ */
+export function RelationAntisymmetricCheck(props: IRelationAntisymmetricCheck) {
+  const { matrix } = props
+  const pairs = checkAntisymmetric(matrix)
+  const antisymmetric = pairs.length === 0
+  const stringPairs = pairs.join(", ")
+
+  return (
+    <Box>
+      <Typography>
+        A relation R on a set A is ANTISYMMETRIC if (a,b) and (b,a)
+        are not both in R unless a = b, for all elements a,b ∈ A.
+        <br />
+        {"Let A = {1, 2, 3, 4}"}
+      </Typography>
+      {antisymmetric
+        ? <Typography>
+          The above relation is ANTISYMMETRIC.
+        </Typography>
+        : <Typography>
+          The above relation is NOT ANTISYMMETRIC because {stringPairs} are in the relation.
+        </Typography>
+      }
+    </Box>
+  )
+}
