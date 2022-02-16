f2388d731b04ace982bc8c2daebbed5cc0b07e55
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-16 00:30:48 +0000

    Add comments

--- src/components/drag-and-drop/drag-and-drop-interface.tsx
+++ src/components/drag-and-drop/drag-and-drop-interface.tsx
@@ -1,12 +1,12 @@
 export interface IItem {
-  id: string,
-  content: string,
-  order: number,
-  rowIndex: number, // row index
-  colIndex: number // column index,
+  id: string,       // Id of the drag and drop item
+  content: string,  // Content (text) of the drag and drop item
+  order: number,    // Order of the drag and drop item to display in
+  rowIndex: number, // Row index for the relation
+  colIndex: number  // Column index for the relation
 }
 
 export interface IState {
-  items: IItem[],
-  selected: IItem[]
+  items: IItem[],     // List of items
+  selected: IItem[]   // List of selected items
 }
