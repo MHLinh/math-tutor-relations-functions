32ec7b0c605bd7de7a9542d451d4f39a91a8bcfc
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-23 17:33:08 +0000

    Add sliders for changing size of sets

---
+++ src/utils/generate-slider-marks.ts
@@ -0,0 +1,22 @@
+interface Mark {
+  value: number,
+  label: string
+}
+
+/**
+ * Function to generate marks for slider
+ * @param step - the step between marks
+ * @param min - the min mark
+ * @param max - the max mark
+ * @returns an array of marks
+ */
+export function generateSliderMarks(step: number, min: number, max: number): Mark[] {
+  const marks: Mark[] = []
+  for(let i = min; i <= max; i += step) {
+    marks.push({
+      value: i,
+      label: `${i}`
+    })
+  }
+  return marks
+}
