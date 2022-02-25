c419c0015a4828ed51003fe44dccb2d74e286f2d
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-02-25 00:06:14 +0000

    Add helper function for vertical line test

---
+++ src/utils/vertical-line-test.ts
@@ -0,0 +1,11 @@
+/**
+ * A function to check if there are multiple points on the graph in a vertical line.
+ * @param points - the x position
+ * @param points - the point on the graph
+ * @returns whether there are multiple points in a vertical line.
+ */
+export function verticalLineTest(position :number, points: number[][]): boolean {
+  const filtered = points.filter(point => point[0] === position)
+
+  return filtered.length > 1
+}
