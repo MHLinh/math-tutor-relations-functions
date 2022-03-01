51062f41c0842d6051ecf6c6f8c9b6ed1daa41a0
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-03-01 17:04:13 +0000

    Add study function navigation

---
+++ src/pages/function-transformation/function-transformation-navigation-page.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import {
+  Container,
+} from "@mui/material"
+import { FunctionTransformationNavigation } from "components"
+
+/**
+ * Displays the page with navigation buttons
+ * for studying the quadratic function.
+ */
+export function FunctionTransformationNavigationPage() {
+  return (
+    <Container>
+      <FunctionTransformationNavigation />
+    </Container>
+  )
+}
