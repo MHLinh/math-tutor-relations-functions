6b8e48be80ee2d13ede09776d93af0561e8041c1
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-03-05 22:30:26 +0000

    Add edge case handling

--- src/utils/generate-slider-marks.ts
+++ src/utils/generate-slider-marks.ts
@@ -13,6 +13,18 @@ interface Mark {
  * @returns an array of marks
  */
 export function generateSliderMarks(step: number, min: number, max: number): Mark[] {
+  if(min > max) {
+    return []
+  }
+
+  if(step === 0) {
+    return [
+      { value: min, label: `${min}` },
+      { value: max, label: `${max}` }
+    ]
+
+  }
+
   const marks: Mark[] = []
   for(let i = min; i <= max; i = roundToTwoDecimal(i + step)) {
     marks.push({
