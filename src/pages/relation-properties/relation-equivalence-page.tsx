8fc8fc4f4da6b2cdaec254ce5dfbde7c03447d0b
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-03-13 23:18:23 +0000

    Add study equivalence relation

---
+++ src/pages/relation-properties/relation-equivalence-page.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  Container,
+} from "@mui/material"
+import { RelationEquivalence } from "components"
+
+/**
+ * Displays the page for studying the equivalence relation property.
+ */
+export function RelationEquivalencePage() {
+  return (
+    <Container>
+      <RelationEquivalence />
+    </Container>
+  )
+}
