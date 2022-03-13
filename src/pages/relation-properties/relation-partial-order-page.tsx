61b7e5735e9aa29da173135a8b9a91fca44a51b6
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-03-13 23:18:33 +0000

    Add study partial order

---
+++ src/pages/relation-properties/relation-partial-order-page.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  Container,
+} from "@mui/material"
+import { RelationPartialOrder } from "components"
+
+/**
+ * Displays the page for studying the partial order property.
+ */
+export function RelationPartialOrderPage() {
+  return (
+    <Container>
+      <RelationPartialOrder />
+    </Container>
+  )
+}
