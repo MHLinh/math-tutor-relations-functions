a5bcf9647d4c721c05e8b8bc5c9b8e203b38cf45
Hoang Linh Mai <linhmaihoang2905@gmail.com>
2022-03-13 23:18:44 +0000

    Add study total order

---
+++ src/pages/relation-properties/relation-total-order-page.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import {
+  Container,
+} from "@mui/material"
+import { RelationTotalOrder } from "components"
+
+/**
+ * Displays the page for studying the total order property.
+ */
+export function RelationTotalOrderPage() {
+  return (
+    <Container>
+      <RelationTotalOrder />
+    </Container>
+  )
+}
